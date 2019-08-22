9a12d078731d2db202836869215c73fb3054c4d0
Jan Miguel Figueiredo-Larsen <janmiguelfl@gmail.com>
2019-08-22 16:05:56 +0200

    added styling for show pages game_session and text_paragraph and some other stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_080752) do
+ActiveRecord::Schema.define(version: 2019_08_22_131446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_080752) do
     t.bigint "text_paragraph_id"
     t.bigint "game_id"
     t.integer "number_of_inputs"
-    t.integer "number_of_attempts"
+    t.integer "number_of_right_inputs"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_game_sessions_on_game_id"
