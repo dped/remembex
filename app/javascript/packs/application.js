0eeb3d9ba1ee5a99fdd1cbe5fb93760a98d4e395
Viktoria Zubchenko <Zubchenko0508@gmail.com>
2019-08-26 17:23:18 +0200

    changes in full_text: index, show and new

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,24 @@
 import "bootstrap";
+
+$(".btn-not-passed-text").hide()
+
+$(".btn-passed-text").click(function() {
+  $(this).toggle()
+  $(this).siblings().toggle()
+});
+
+$(".btn-not-passed-text").click(function() {
+  $(this).toggle()
+  $(this).siblings().toggle()
+});
+
+$(".information-window").hide()
+
+$(".decoration-for-navbar").click(function() {
+  $(".information-window").show()
+});
+
+
+$(".btn-close-info").click(function() {
+  $(".information-window").hide()
+});
